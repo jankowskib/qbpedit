92d156b9a2a82855bc74a3df78ca1ccc3180f6c2
Bartosz J <thug1337@gmail.com>
2013-05-07 12:43:38 +0200

    Initial commit

---
+++ qpbedit.c
@@ -0,0 +1,87 @@
+/*   qpbedit.c
+ *      
+ *   Copyright 2013 Bartosz Jankowski
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+
+
+static void usage(char **argv)
+{
+	printf("Usage: %s [options] file\n"
+		"\t-m,\t--model [text]\t\tchange model name\n"
+		"\t-p,\t--producer [text]\tchange producer name\n"
+		"\t-u,\t--update-timestamp\tupdate timestamp to current one\n"
+		"\t-h,\t--help\t\t\tdisplay this help and exit\n"
+		, argv[0]
+	);
+}
+
+int main(int argc, char *argv[])
+{
+	char * model, * prod;
+	int c, t = 0, i_opt = 0;
+	const struct option s_opt[] = 
+	{
+		{"model",			required_argument,	0,	'm'	},
+		{"producer",		required_argument,	0,	'p'	},
+		{"update-timestamp",no_argument,		0,	'u'	},
+		{"help",			no_argument,		0,	'h'	},
+		{0,					0,					0,	0	}
+	};
+	
+	while((c = getopt_long(argc, argv, "m:p:uh", s_opt, &i_opt)) != -1) 
+	{
+		switch(c) 
+		{
+		case 'm':
+			model = optarg;
+			break;
+		case 'p':
+			prod = optarg;
+			break;
+		case 'u':
+			t = time(NULL);
+			break;
+		case 'h':
+			usage(argv);
+			break;
+		default:
+			printf("Unknown parameter. Type %s --help to show usage\n", argv[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+		 
+	if (optind < argc )
+	{
+	//in = fopen(argv[optind], "rb");
+		printf ("Input file: %s\n", argv[optind]);
+	}
+	else
+	{
+		printf("Specify input file.\n");
+		exit(1);
+	}
+	printf("=====================================\n"
+		   "Quick Build.Prop Edit v. 0.1 by lolet\n"
+		   "=====================================\n");
+	if(model) printf("Model : %s\n", model);
+	if(prod) printf("Producer : %s\n", prod);
+	if(t) printf("Timestamp : %d\n", t);
+	
+	return 0;
+}
+
